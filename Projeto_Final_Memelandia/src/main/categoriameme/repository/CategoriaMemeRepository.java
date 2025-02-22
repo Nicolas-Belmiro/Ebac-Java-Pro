dea7389dcc92fc427a7a78e100eb15c12862e102
Nicolas-Belmiro <122122745+Nicolas-Belmiro@users.noreply.github.com>
2025-02-22 11:16:48 -0300

    Projeto final do curso da EBAC
    #Java Back-End Final Project Specialist Ebac | MemeLand
    
    
    Documentação
    
    
    Visão geral
    
    Este projeto Memelândia foi o último desafio proposto no curso Ebac Java Back-End Specialist, onde tínhamos um serviço monolítico e tivemos que desmembrá-lo e fazer 3 microsserviços deles, criando assim um microsserviço Usuário, CategoriaMeme e Meme.
    
    Pré-requisitos
    
    
    JDK (Java Development Kit) - Versão 17 foi usada.
    Maven ou Gradle (para gerenciamento de dependências) - Maven foi usado.
    Dependências: SpringBoot, Actuator, openfeign, zookeeper e PostgreSQL -> o banco de dados pode ser qualquer outro, basta alterar as configurações corretamente.
    Zipkin também foi usado, o que permite que os desenvolvedores criem rastreamento funcional em sua base de código personalizada para obter insights sobre o comportamento específico do aplicativo.

---
+++ Projeto_Final_Memelandia/src/main/categoriameme/repository/CategoriaMemeRepository.java
@@ -0,0 +1,7 @@
+package repository;
+
+import entity.CategoriaMeme;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoriaMemeRepository extends CrudRepository<CategoriaMeme, Long> {
+}
